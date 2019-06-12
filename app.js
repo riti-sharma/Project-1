3723eab58626d3c0a2b11da316b80e31582a1965
riti-sharma <riti.sharma11@yahoo.com>
2019-06-12 11:14:49 -0400

    update

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const button = document.querySelector('button')
 const div = document.querySelector('div')
 const apikey = "46c85d02ac3500f88225b35170f06a6c"
 const doctorInfo = document.querySelector('.doctor-info');
+const button2 = document.getElementsByTagName('button')[1];
+const input2 = document.getElementsByTagName('input')[1];
 
 button.addEventListener('click', async () => {
   const doctor = input.value;
@@ -17,5 +19,19 @@ button.addEventListener('click', async () => {
   }
 
   doctorInfo.appendChild(info);
+})
 
+button2.addEventListener('click', async () => {
+  const query = input2.value;
+  const response = await axios.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&user_key=${apikey}`)
+  console.log(response);
+  const info = document.createElement('div');
+  info.innerHTML = `<h2>Top 3 Results: </h2>`
+  for (let i = 0; i < 3; i++) {
+    info.innerHTML += `
+      <p> ${response.data.data[i].profile.bio}</p>
+      `
+  }
+
+  doctorInfo.appendChild(info);
 })
